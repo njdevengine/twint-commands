8bcfbdcc2a20fbf0ed315d27f3200417091bd2ad
njdevengine <24538036+njdevengine@users.noreply.github.com>
2020-07-02 09:00:42 -0400

    Update get_by_dateranges.py

--- get_by_dateranges.py
+++ get_by_dateranges.py
@@ -14,11 +14,21 @@ for n in years:
         ranges.append(get_month_day_range(datetime.date(n,i,15)))
         
 queries = []
+filenames = []
 tag = "#enterhastag or enter phrase"
 filename = "YOUR_FILENAME_HERE"
-
-for i in ranges:
-    search = 'twint -s "'+tag+'" --since '+i[0]+' --until '+i[1]+' -o '+filename+'.csv --csv'
-    queries.append(search)
     
-queries
+for i in ranges:
+    search = 'twint -s "'+tag+'" --since '+i[0]+' --until '+i[1]+' -o '+filename+'_'+i[0]+'.csv --csv'
+    filenames.append(filename+'_'+i[0]+'.csv')
+    queries.append(search)
+
+import time
+import os
+for i in queries:
+    print(i)
+    start_time = time.time()
+    try:
+        os.system(i)
+        print("--- %s seconds ---" % (time.time() - start_time))
+    except Exception as e: print(e)
